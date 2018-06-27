52a8cb7e88d116bbe2df80caa1501c2f1e182eee
Luke Gaudreau <gaudreal@bc.edu>
2018-06-27 14:23:13 -0400

    Remove unused variable scopename

--- js/noresults.module.js
+++ js/noresults.module.js
@@ -4,8 +4,6 @@ angular
         var vm = this;
         vm.term = vm.parentCtrl.term;
         vm.scope = vm.parentCtrl.searchStateService.searchObject.scope;
-        vm.scopename = {bcl:"Books", pci:"Article"};
-
     }])
     .component('prmNoSearchResultAfter', {
         bindings: { parentCtrl: '<'},
