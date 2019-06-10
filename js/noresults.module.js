5a4453c4b456b6033d7c077d6bb189f22b7473f9
Luke Gaudreau <gaudreal@bc.edu>
2019-06-10 10:19:09 -0400

    Don't show query for advanced search

--- js/noresults.module.js
+++ js/noresults.module.js
@@ -3,7 +3,15 @@ angular
     .controller('prmNoSearchResultsAfterController', [function () {
         var vm = this;
         vm.term = vm.parentCtrl.term;
+        vm.mode = vm.parentCtrl.searchStateService.searchObject.mode;
         vm.scope = vm.parentCtrl.searchStateService.searchObject.scope;
+        vm.showQuery = showQuery;
+        function showQuery() {
+            if(vm.parentCtrl.isSearchWithFilter == false && vm.mode != "advanced")
+                return true;
+            else
+                return false;
+        }
     }])
     .component('prmNoSearchResultAfter', {
         bindings: { parentCtrl: '<'},
