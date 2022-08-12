a1393e0d6cf8bb5f08989d7189ac1eb0f62c280c
colfords <scot.colford@bc.edu>
2022-08-12 11:59:17 -0400

    Updated for Primo Angular 1.8 upgrade. ILLiad form added as suggestion link.

--- js/noresults.module.js
+++ js/noresults.module.js
@@ -1,17 +1,21 @@
 angular
     .module('bc-noresults', [])
     .controller('prmNoSearchResultsAfterController', [function () {
-        var vm = this;
-        vm.term = vm.parentCtrl.term;
-        vm.mode = vm.parentCtrl.searchStateService.searchObject.mode;
-        vm.scope = vm.parentCtrl.searchStateService.searchObject.scope;
-        vm.showQuery = showQuery;
-        function showQuery() {
-            if(vm.parentCtrl.isSearchWithFilter == false && vm.mode != "advanced")
-                return true;
-            else
-                return false;
-        }
+		this.$onInit = function () {
+			{
+				var vm = this;
+				vm.term = vm.parentCtrl.term;
+				vm.mode = vm.parentCtrl.searchStateService.searchObject.mode;
+				vm.scope = vm.parentCtrl.searchStateService.searchObject.scope;
+				vm.showQuery = showQuery;
+				function showQuery() {
+					if(vm.parentCtrl.isSearchWithFilter == false && vm.mode != "advanced")
+						return true;
+					else
+						return false;
+				}
+			}
+		};
     }])
     .component('prmNoSearchResultAfter', {
         bindings: { parentCtrl: '<'},
